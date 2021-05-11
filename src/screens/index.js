1d5c5b1dd933d4548b6560f4c336f108a58f905f
Senior Javascript Developer <silver.19920220@gmail.com>
2021-05-11 17:18:46 +0800

    feat: added parsing image from library

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import Toast from 'react-native-toast-message';
 
 import BubbleSheetAPI from '../common/api/bubble-sheet';
-import { ToastStatus } from '../common/enum/toast.enum';
+import {ToastStatus} from '../common/enum/toast.enum';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     backgroundColor: '#3179f6',
+    marginVertical: 10,
   },
   textWhite: {
     color: 'white',
@@ -29,11 +30,11 @@ function BubbleSheetScreen() {
 
   useEffect(() => {
     if (imageData) {
+      setScores([]);
       BubbleSheetAPI.scanBubbleSheet(imageData)
         .then(res => {
-          setScores(res);
-          console.log(res);
           if (res.length > 0) {
+            setScores(res);
             Toast.show({
               text1: 'Success',
               text2: `${res.length} answers found.`,
@@ -58,21 +59,60 @@ function BubbleSheetScreen() {
     }
   }, [imageData]);
 
-  const onPressTakeImage = () => {
+  const onPressTakeImageFromCamera = () => {
     ImagePicker.openCamera({
       width: 800,
       height: 600,
       includeBase64: true,
       cropping: true,
-    }).then(image => {
-      setImageData(`data:${image.mime};base64,${image.data}`);
-    });
+    })
+      .then(image => {
+        setImageData(`data:${image.mime};base64,${image.data}`);
+      })
+      .catch(error => {
+        Toast.show({
+          text1: 'Error',
+          text2: 'You have cancelled parsing bubble sheet.',
+          type: ToastStatus.Error,
+        });
+      });
+  };
+
+  const onPressTakeImageFromLibrary = () => {
+    ImagePicker.openPicker({
+      width: 800,
+      height: 600,
+      includeBase64: true,
+      cropping: true,
+    })
+      .then(image => {
+        setImageData(`data:${image.mime};base64,${image.data}`);
+      })
+      .catch(error => {
+        Toast.show({
+          text1: 'Error',
+          text2: 'You have cancelled parsing bubble sheet.',
+          type: ToastStatus.Error,
+        });
+      });
   };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.btnPrimary} onPress={onPressTakeImage}>
-        <Text style={styles.textWhite}>Take Bubble Sheet Image</Text>
+      <Text>{scores.join(', ')}</Text>
+      <TouchableOpacity
+        style={styles.btnPrimary}
+        onPress={onPressTakeImageFromCamera}>
+        <Text style={styles.textWhite}>
+          Take Bubble Sheet Image from Camera
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.btnPrimary}
+        onPress={onPressTakeImageFromLibrary}>
+        <Text style={styles.textWhite}>
+          Take Bubble Sheet Image from Library
+        </Text>
       </TouchableOpacity>
       <Toast ref={ref => Toast.setRef(ref)} />
     </View>
